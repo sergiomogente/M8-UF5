0aaf02a0c6a24a8e32e9a7bc87944fd0b1fbb642
sergi <sergiomogente99@gmail.com>
2023-05-25 10:52:46 +0200

    first commit

---
+++ Bancs.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bancs;
+
+/**
+ *
+ * @author sergi
+ */
+public class Bancs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+    }
+}
+# M8-UF5
